1039fd58b4eff268c41444cfe734e27f15ea5e5f
Andre Pereira de Carvalho <andreesoul@gmail.com>
2025-06-01 11:03:27 -0300

    Alpine.js for the success flash message

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
 <head>
   <title>Laravel 10 Task List App</title>
   <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
-
+  <script src="//unpkg.com/alpinejs" defer></script>
     {{-- blade-formater-disable --}}
     <style type="text/tailwindcss">
         .btn {
@@ -31,13 +31,23 @@
 
 <body class="container mx-auto mt-10 mb-10 max-w-lg">
   <h1 class= "text-2xl mb-4">@yield('title')</h1>
-  <div>
+  <div x-data="{ flash: true }">
     @if(session('success'))
-        <div class="mb-10 rounded border-green-400 bg-green-100 px-4 py-3 text-lg text-green-700">
+        <div  x-show="flash"
+        class="relative mb-10 rounded border-green-400 bg-green-100 px-4 py-3 text-lg text-green-700"
+            role="alert">
             <strong>Success!</strong>
             <div>
                 {{session('success')}}
             </div>
+
+            <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                stroke-width="1.5" @click="flash = false"
+                stroke="currentColor" class="h-6 w-6 cursor-pointer">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+            </svg>
+            </span>
         </div>
     @endif
     @yield('content')
